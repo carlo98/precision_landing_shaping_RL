913b618ae4dbaecaa4640bfa7fd07e866ebcfd25
Carlo Cena <carlo.cena@protonmail.com>
2021-12-02 18:34:53 +0100

    Stop for noetic

--- shared/ros_packages/px4_ros_extended/src_py/env_wrapper.py
+++ shared/ros_packages/px4_ros_extended/src_py/env_wrapper.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
 from rewards import Reward
+import rclpy
 
 # ROS dep
 from std_msgs.msg import Int64
-from std_srvs.srv import Empty
+from gazebo_msgs.msg import Empty
 from gazebo_msgs.msg import EntityState
-from gazebo_msgs.srv import SetEntityState
+from gazebo_msgs.srv import SpawnEntity
+from gazebo_msgs.srv import DeleteEntity
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Twist
 
@@ -42,6 +44,7 @@ class EnvWrapperNode:
         self.max_vel_z = max_vel_z
         self.max_vel_xy = max_vel_xy
         self.state_shape = state_shape
+        self.respawn_flag = False
 
     def vehicle_odometry_callback(self, obs):
         # 0--x, 1--y, 2--z, 3--vx, 4--vy
@@ -79,6 +82,7 @@ class EnvWrapperNode:
                (np.abs(self.state[3]) > self.eps_vel_xy or np.abs(self.state[4]) > self.eps_vel_xy)
 
     def reset_env(self):  # Used for synchronization with gazebo
+        self.reset_world()
         play_reset_msg = Float32MultiArray()
         play_reset_msg.data = [1.0, 1.0]
         self.play_reset_publisher.publish(play_reset_msg)
@@ -106,11 +110,32 @@ class EnvWrapperNode:
             self.reset = False
 
     # The following methods are not used, because they create problems with PX4 calibration
-    """
     def set_pose_model(self):
         self.service_call("/pause_physics")
-        self.service_call_set_state("/demo/set_entity_state")  # or reset_world
+        self.service_call_set_state("/demo/set_entity_state")
         self.service_call("/unpause_physics")
+
+    def reset_world(self):
+        self.service_call("/pause_physics")
+        self.service_call("/reset_world")
+        self.service_call("/unpause_physics")
+
+    def respawn(self):
+        self.respawn_flag = True
+        #self.service_call("/pause_physics")
+        self.service_delete("/delete_entity")
+        self.service_spawn("/spawn_entity")
+        #self.service_call("/unpause_physics")
+        self.respawn_flag = False
+
+    def service_call(self, name_service):
+        respawn = self.node.create_client(Empty, name_service)
+        while not respawn.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info('service not available, waiting again...')
+        req = Empty.Request()
+        fut = respawn.call_async(req)
+        while not fut.done():
+            pass
 
     def service_call_set_state(self, name_service):
         state_msg = EntityState()
@@ -140,12 +165,37 @@ class EnvWrapperNode:
         while not fut.done():
             pass
 
-    def service_call(self, name_service):
-        respawn = self.node.create_client(Empty, name_service)
+    def service_spawn(self, name_service):
+        f_sdf = open("/src/shared/PX4-Autopilot/Tools/sitl_gazebo/models/iris/iris.sdf", "r")
+        sdf = f_sdf.read()
+        f_sdf.close()
+        print(sdf)
+        pose_msg = Pose()
+        pose_msg.position.x = 0.0
+        pose_msg.position.y = 0.0
+        pose_msg.position.z = 0.2
+        pose_msg.orientation.x = 0.0
+        pose_msg.orientation.y = 0.0
+        pose_msg.orientation.z = 0.0
+        pose_msg.orientation.w = 0.0
+        respawn = self.node.create_client(SpawnEntity, name_service)
         while not respawn.wait_for_service(timeout_sec=1.0):
-            self.node.get_logger().info('service not available, waiting again...')
-        req = Empty.Request()
+            self.node.get_logger().info(name_service + ': service not available, waiting again...')
+        req = SpawnEntity.Request(name='iris', xml=sdf, robot_namespace='', reference_frame="world",
+                                  initial_pose=pose_msg)
         fut = respawn.call_async(req)
+        rclpy.spin_until_future_complete(self.node, fut)
         while not fut.done():
-            pass
-   """
+            print(name_service)
+
+    def service_delete(self, name_service):
+        respawn = self.node.create_client(DeleteEntity, name_service)
+        while not respawn.wait_for_service(timeout_sec=1.0):
+            self.node.get_logger().info(name_service + ': service not available, waiting again...')
+        req = DeleteEntity.Request(name="iris")
+        fut = respawn.call_async(req)
+        rclpy.spin_until_future_complete(self.node, fut)
+        #while not fut.done():
+        #    print(name_service)
+
+
