5b89a246bc95d25aff9d87f953bfed60bae8cb34
Carlo Cena <carlo.cena@protonmail.com>
2022-02-22 10:36:58 +0000

    Added dockerfile for foxy, updated run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -13,6 +13,16 @@ then
            -m="3g" \
            -e DISPLAY \
            --name=RL_dashing px4io/px4-dev-ros2-dashing bash
+elif [ $1 = 'build_foxy' ]
+then
+    docker build -t px4io/px4-dev-ros2-foxy . -f Dockerfile_foxy
+    docker run --privileged -it --net=host \
+	   -v /home/carlo/Desktop/AI_in_Industry/Project/pl_pirl/shared:/src/shared:rw \
+	   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+	   -v /home/carlo/Desktop/AI_in_Industry/Project/pl_pirl/.bashrc:/root/.bashrc \
+           -m="3g" \
+           -e DISPLAY \
+           --name=RL_foxy px4io/px4-dev-ros2-foxy bash
 fi
 
 cp $(xauth info | head -n 1 | cut -d' ' -f9) ~/.Xauthority
@@ -22,4 +32,7 @@ if [ $1 = 'run' ]
 then
     docker start RL_dashing
     docker exec -it RL_dashing bash
-fi
+elif [ $1 = 'run_foxy' ]
+then
+    docker start RL_foxy
+    docker exec -it RL_foxy bash
